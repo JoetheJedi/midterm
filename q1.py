cea09dccc067a456e8ba4d0f0506b06f6f959b85
Joseph Gonzalez <jgonzalez259@mail.valenciacollege.edu>
2016-04-14 04:04:51 -0400

    There we go
    Tested by adding numbers in the runner file and the calculations were
    right, so hope this is correct

--- q1.py
+++ q1.py
@@ -9,12 +9,10 @@
 #   Example 4: if your balance is 8, then your program should return 8. 
 
 def computeMinimumPayment( balance ):
-    if balance==8:#in this line the minimum payment would exceed the balance which is 8 so we return 8
-        return '8'
-    if balance==600:#In this line the balance does not exceed the minimum payment so we multiply the balance which is 600 by .021 which will give us 2.1% of 600
-        return 600*.021
-    if balance==1000:#In this line the balance does not exceed the minimum payment so we multiply the balance which is 1000 by .021 which will give us 2.1% of 1000
-        return 1000*.021
-    if balance==25:#in this line since $10 is greater than 2.1% of 25 we will return 10 instead of 25*.021
-        return '10'
-    
+    if balance*.021>10:
+        balance=balance*.021
+        return balance
+    else:
+        balance=10
+        return balance
+
