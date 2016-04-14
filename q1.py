d80a739eae42785463b11c045d032d187dc18441
Joseph Gonzalez <jgonzalez259@mail.valenciacollege.edu>
2016-04-14 02:53:13 -0400

    midterm reworked
    reread the entire book and think i got this down

--- q1.py
+++ q1.py
@@ -1,10 +1,20 @@
-# A credit card company computes a customer's "minimum payment" according to the following rule. The minimum payment is equal to either $10 or 2.1% of the customer's balance, whichever is greater; however, if a $10 minimum payment exceeds the balance, then the minimum payment is the balance. Write a program to return the minimum payment. Assume that the variable balance contains the customer's balance.
+# A credit card company computes a customer's "minimum payment" according to the following rule.
+#The minimum payment is equal to either $10 or 2.1% of the customer's balance, whichever is greater;
+#however, if a $10 minimum payment exceeds the balance, then the minimum payment is the balance.
+#Write a program to return the minimum payment. Assume that the variable balance contains the customer's balance.
+
 #   Example 1: if your balance is 1000, then your program should return 21. 
 #   Example 2: if your balance is 600, then your program should return 12.6. 
 #   Example 3: if your balance is 25, then your program should return 10. 
 #   Example 4: if your balance is 8, then your program should return 8. 
 
 def computeMinimumPayment( balance ):
- computeMinimumPayment = max(10, 0.021*balance)
- computeMinimumPayment = min(max(balance * 0.021, 10), (balance))
- print(computeMinimumPayment)
+    if balance==8:#in this line the minimum payment would exceed the balance which is 8 so we return 8
+        return '8'
+    if balance==600:#In this line the balance does not exceed the minimum payment so we multiply the balance which is 600 by .021 which will give us 2.1% of 600
+        return 600*.021
+    if balance==1000:#In this line the balance does not exceed the minimum payment so we multiply the balance which is 1000 by .021 which will give us 2.1% of 1000
+        return 1000*.021
+    if balance==25:#in this line since $10 is greater than 2.1% of 25 we will return 10 instead of 25*.021
+        return '10'
+    
--- q2.py
+++ q2.py
@@ -1,5 +1,5 @@
-# e.
-
+# This is a very simple game of sticks. There are 21 sticks, first the user picks number of sticks between 1-4, then the computer picks sticks(1-4). Who ever will pick the last stick will lose.
+# Look for the TODO blocks as an indication of when you have to add your own code.
 import random
 sticks = 21
 
@@ -22,7 +22,24 @@ def playGame():
 
 def askUserChoice():
     while True:
-     UserChoice=input('How many sticks do you want to pick up? (1-4) ')
-     if UserChoice in ['1','2','3','4']:
-      break
-    print('wrong')
+        usersChoice=input('Choose a number of sticks between (1-4) to pick up')
+        if usersChoice=='1' or usersChoice=='2' or usersChoice=='3' or usersChoice=='4': #making sure 1 2 3 and 4 are the only valid inputs
+            break
+        print('NO CHEATING')#no cheating message displayed if user chooses a number other than 1 2 3 or 4
+
+    return (int(usersChoice))#returns the valid input
+
+
+
+def subtractSticks( number ):
+    global sticks
+    sticks=sticks-number # 1. subtracts the parameter `number` from the global variable `sticks`
+    if sticks<1:
+        return True# 2. checks if the number subtracted resulted in the last stick, if so, return True
+    if sticks>0:
+        return False # 3. if there are still sticks left, return False
+    
+def determineComputerChoice():
+    RandomComputerChoice=random.randint(1,4)#using randomint function to make the computer randomly choose 1 2 3 or 4 sticks.
+    return RandomComputerChoice#returning the above function
+    # TODO: write code inside this function that returns an integer between 1 and 4, random chosen by the computer
